79dc65adaa86e0639316f3bac18ea3ed45385c55
Kobayashi123 <g2053417@cc.kyoto-su.ac.jp>
2022-12-19 23:40:24 +0900

    add: implement header and footer description sections

--- CSV2HTML_by_Java_Skeleton/csv2html/Writer.java
+++ CSV2HTML_by_Java_Skeleton/csv2html/Writer.java
@@ -13,14 +13,13 @@ import utility.StringUtility;
 /**
  * ライタ：情報のテーブルをHTMLページとして書き出す。
  */
-public class Writer extends IO
-{
+public class Writer extends IO {
 	/**
 	 * ライタのコンストラクタ。
+	 *
 	 * @param aTable テーブル
 	 */
-	public Writer(Table aTable)
-	{
+	public Writer(Table aTable) {
 		super(aTable);
 
 		return;
@@ -29,10 +28,8 @@ public class Writer extends IO
 	/**
 	 * HTMLページを基にするテーブルからインデックスファイル(index.html)に書き出す。
 	 */
-	public void perform()
-	{
-		try
-		{
+	public void perform() {
+		try {
 			Attributes attributes = this.attributes();
 			String fileStringOfHTML = attributes.baseDirectory() + attributes.indexHTML();
 			File aFile = new File(fileStringOfHTML);
@@ -45,46 +42,150 @@ public class Writer extends IO
 			this.writeFooterOn(aWriter);
 
 			aWriter.close();
+		} catch (UnsupportedEncodingException | FileNotFoundException anException) {
+			anException.printStackTrace();
+		} catch (IOException anException) {
+			anException.printStackTrace();
 		}
-		catch (UnsupportedEncodingException | FileNotFoundException anException) { anException.printStackTrace(); }
-		catch (IOException anException) { anException.printStackTrace(); }
 
 		return;
 	}
 
 	/**
 	 * 属性リストを書き出す。
+	 *
 	 * @param aWriter ライタ
 	 */
-	public void writeAttributesOn(BufferedWriter aWriter)
-	{
+	public void writeAttributesOn(BufferedWriter aWriter) {
 		return;
 	}
 
 	/**
 	 * フッタを書き出す。
+	 *
 	 * @param aWriter ライタ
 	 */
-	public void writeFooterOn(BufferedWriter aWriter)
-	{
+	public void writeFooterOn(BufferedWriter aWriter) {
+		String footer = """
+				<hr>
+				<div class="right-small">Created by csv2html.Translator on 2021/10/21 at 13:04:23</div>
+				</body>
+				</html>
+
+				""";
+
+		try {
+			aWriter.write(footer);
+		} catch (IOException anException) {
+			anException.printStackTrace();
+		}
 		return;
 	}
 
 	/**
 	 * ヘッダを書き出す。
+	 *
 	 * @param aWriter ライタ
 	 */
-	public void writeHeaderOn(BufferedWriter aWriter)
-	{
+	public void writeHeaderOn(BufferedWriter aWriter) {
+		String header = """
+				<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+				<html lang="ja">
+				<head>
+				<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+				<meta http-equiv="Content-Style-Type" content="text/css">
+				<meta http-equiv="Content-Script-Type" content="text/javascript">
+				<meta name="keywords" content="Smalltalk,Oriented,Programming">
+				<meta name="description" content="Prime Ministers">
+				<meta name="author" content="AOKI Atsushi">
+				<link rev="made" href="http://www.cc.kyoto-su.ac.jp/~atsushi/">
+				<link rel="index" href="index.html">
+				<style type="text/css">
+				<!--
+				body {
+					background-color : #ffffff;
+					margin : 20px;
+					padding : 10px;
+					font-family : serif;
+					font-size : 10pt;
+				}
+				a {
+					text-decoration : underline;
+					color : #000000;
+				}
+				a:link {
+					background-color : #ffddbb;
+				}
+				a:visited {
+					background-color : #ccffcc;
+				}
+				a:hover {
+					background-color : #dddddd;
+				}
+				a:active {
+					background-color : #dddddd;
+				}
+				div.belt {
+					background-color : #eeeeee;
+					padding : 0px 4px;
+				}
+				div.right-small {
+					text-align : right;
+					font-size : 8pt;
+				}
+				img.borderless {
+					border-width : 0px;
+					vertical-align : middle;
+				}
+				table.belt {
+					border-style : solid;
+					border-width : 0px;
+					border-color : #000000;
+					background-color : #ffffff;
+					padding : 0px 0px;
+					width : 100%;
+				}
+				table.content {
+					border-style : solid;
+					border-width : 0px;
+					border-color : #000000;
+					padding : 2px 2px;
+				}
+				td.center-blue {
+					padding : 2px 2px;
+					text-align : center;
+					background-color : #ddeeff;
+				}
+				td.center-pink {
+					padding : 2px 2px;
+					text-align : center;
+					background-color : #ffddee;
+				}
+				td.center-yellow {
+					padding : 2px 2px;
+					text-align : center;
+					background-color : #ffffcc;
+				}
+				-->
+				</style>
+				<title>Prime Ministers</title>
+				</head>
+				""";
+
+		try {
+			aWriter.write(header);
+		} catch (IOException anException) {
+			anException.printStackTrace();
+		}
 		return;
 	}
 
 	/**
 	 * ボディを書き出す。
+	 *
 	 * @param aWriter ライタ
 	 */
-	public void writeTableBodyOn(BufferedWriter aWriter)
-	{
+	public void writeTableBodyOn(BufferedWriter aWriter) {
 		this.writeAttributesOn(aWriter);
 		this.writeTuplesOn(aWriter);
 
@@ -93,10 +194,10 @@ public class Writer extends IO
 
 	/**
 	 * タプル群を書き出す。
+	 *
 	 * @param aWriter ライタ
 	 */
-	public void writeTuplesOn(BufferedWriter aWriter)
-	{
+	public void writeTuplesOn(BufferedWriter aWriter) {
 		return;
 	}
 }
